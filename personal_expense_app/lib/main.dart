da496fe84d0156a1831c997e02d34c1537af024c
Raiyan <raiyan.sharif.1234@gmail.com>
2021-02-23 15:47:44 +0600

    DateFormate added

--- personal_expense_app/lib/main.dart
+++ personal_expense_app/lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:personal_expense_app/transaction.dart';
+import 'package:intl/intl.dart';
 
 void main() {
   runApp(MyApp());
@@ -41,18 +42,46 @@ class MyApp extends StatelessWidget {
               ),
             ),
             Column(
-              children: transactions.map((tx){
+              children: transactions.map((tx) {
                 return Card(
                   child: Row(
                     children: [
                       Container(
-                        margin: EdgeInsets.symmetric(vertical: 10, horizontal: 15),
-                        child: Text(tx.amount.toString()),
+                        margin: EdgeInsets.symmetric(
+                          vertical: 10,
+                          horizontal: 15,
+                        ),
+                        decoration: BoxDecoration(
+                            border: Border.all(
+                          color: Colors.purple,
+                          width: 2,
+                        )),
+                        padding: EdgeInsets.all(10),
+                        child: Text(
+                          '\$ ${tx.amount}',
+                          style: TextStyle(
+                              fontWeight: FontWeight.bold,
+                              fontSize: 20,
+                              color: Colors.purple),
+                        ),
                       ),
                       Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
                         children: [
-                          Text(tx.title),
-                          Text(tx.date.toString())
+                          Text(
+                            tx.title,
+                            style: TextStyle(
+                                fontWeight: FontWeight.bold,
+                                fontSize: 16,
+                                color: Colors.black
+                            ),
+                          ),
+                          Text(
+                            DateFormat('dd-LLL-yyyy').format(tx.date),
+                            style: TextStyle(
+                              color: Colors.grey
+                            ),
+                          ),
                         ],
                       )
                     ],
